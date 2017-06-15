0d34e2a6eff7863a9d185de112a23dad72e9b5dc
Hardy <h6pham8@gmail.com>
2017-06-15 10:07:32 -0700

    Add header menu functionality.

--- public/javascripts/hardypham.js
+++ public/javascripts/hardypham.js
@@ -1,3 +1,26 @@
-/**
- * Created by Hardy on 6/3/17.
- */
+var MainMenu = {
+  menu: {
+      experience: $("#experience"),
+      projects: $("#projects"),
+      education: $("#education")
+
+  },
+  init: function(){
+      m = this.menu;
+      this.bindClickAction(m.experience, $("#experienceSection"));
+      this.bindClickAction(m.projects, $("#projectsSection"));
+      this.bindClickAction(m.education, $("#educationSection"));
+      this.scrollAction();
+  },
+  bindClickAction: function(menu, destination) {
+      menu.click(function () {
+          $('html, body').animate({
+              scrollTop: destination.offset().top - 50
+          }, 1000);
+      });
+  }
+};
+
+$(function(){
+    MainMenu.init();
+});
