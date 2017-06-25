2ad7d265e308220103e822f3e06cb4f85aa68676
Hardy <h6pham8@gmail.com>
2017-06-25 23:50:43 +0900

    Add modal action/js for project pictures.

--- public/javascripts/hardypham.js
+++ public/javascripts/hardypham.js
@@ -3,14 +3,16 @@ var MainMenu = {
       experience: $("#experience"),
       projects: $("#projects"),
       education: $("#education")
-
+  },
+  modal: {
+      micromouse: $("#disp-MM")
   },
   init: function(){
       m = this.menu;
       this.bindClickAction(m.experience, $("#experienceSection"));
       this.bindClickAction(m.projects, $("#projectsSection"));
       this.bindClickAction(m.education, $("#educationSection"));
-      this.scrollAction();
+
   },
   bindClickAction: function(menu, destination) {
       menu.click(function () {
@@ -21,6 +23,35 @@ var MainMenu = {
   }
 };
 
-$(function(){
+var Modal = {
+    modal: {
+        micromouse: $("#disp-MM"),
+        triviaTrip: $("#disp-TT"),
+        igs: $("#disp-IGS")
+    },
+    init: function(){
+        modal = this.modal;
+        this.addModalFunctionality();
+        this.bindModal(modal.micromouse, $("#micromouseModalTrigger"));
+        this.bindModal(modal.triviaTrip, $("#triviaTripModalTrigger"));
+        this.bindModal(modal.igs, $("#igsModalTrigger"));
+
+
+    },
+    bindModal: function(modalButton, modalTrigger){
+        modalButton.on('click', function(){
+            console.log('hello');
+            modalTrigger.addClass('is-active');
+        });
+    },
+    addModalFunctionality: function(){
+        $('.modal-background, .modal-close').on('click', function(){
+            $('.modal').removeClass('is-active');
+        });
+    }
+};
+
+$(document).ready(function(){
     MainMenu.init();
+    Modal.init();
 });
